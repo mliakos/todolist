9837a12a65893f3610dc93b18e38dcec3dcdc1d2
Manos Liakos <manosliakoss@gmail.com>
2018-10-28 15:53:37 +0200

    Add backend JWT validation

---
+++ api/validate_token.php
@@ -0,0 +1,65 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: http://localhost/rest-api-authentication-example/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+// include libraries
+include_once 'config/core.php';
+include_once 'libs/BeforeValidException.php';
+include_once 'libs/ExpiredException.php';
+include_once 'libs/SignatureInvalidException.php';
+include_once 'libs/JWT.php';
+use \Firebase\JWT\JWT;
+ 
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+ 
+// get jwt
+$jwt=isset($data->jwt) ? $data->jwt : "";
+ 
+// if jwt !empty
+if($jwt){
+ 
+    // if decoding succeeds, show user details
+    try {
+        // decode jwt
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
+ 
+        // set response code
+        http_response_code(200);
+ 
+        // show user details
+        echo json_encode(array(
+            "message" => "Access granted.",
+            "data" => $decoded->data
+        ));
+ 
+    }
+ 
+    // if decoding fails-> jwt==invalid
+    catch (Exception $e){
+    
+        // set response code
+        http_response_code(401);
+    
+        // tell the user access denied  & show error message
+        echo json_encode(array(
+            "message" => "Access denied.",
+            "error" => $e->getMessage()
+        ));
+    }
+}
+ 
+// error msg if jwt==empty
+else{
+ 
+    // set response code
+    http_response_code(401);
+ 
+    // tell the user access denied
+    echo json_encode(array("message" => "Access denied."));
+}
+?>
